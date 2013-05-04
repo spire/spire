c00a40bc88d9ba41d75c6a5a8961c5724d5e6171
Nathan Collins <nathan.collins@gmail.com>
2013-05-03 19:02:12 -0700

    Add embedders from 'Infer' and 'Check' to 'Syntax'.
    This allows for a simple way of defining pretty printers for types
    other than 'Syntax', by embedding them into 'Syntax'.
    
    @larrytheliquid is this case correct:
    
        embedI (ILamAnn c bi) = SLam (embedIB bi)
    
    ? (And does github recognize the '@' markdown in commit messages?)

---
+++ src/Spire/Expression/Embedding.hs
@@ -0,0 +1,56 @@
+module Spire.Expression.Embedding where
+import Spire.Canonical.Types
+import Spire.Expression.Types
+import Spire.Surface.Types
+
+----------------------------------------------------------------------
+
+embedI :: Infer -> Syntax
+embedI ITT = STT
+embedI ITrue = STrue
+embedI IFalse = SFalse
+embedI (IQuotes s) = SQuotes s
+-- XXX: this is wrong: may need the domain typing info in 'c', but
+-- there is nowhere to put it ... could maybe infer a type for the
+-- 'ILamAnn', and then wrap with 'SAnn'?
+embedI (ILamAnn c bi) = SLam (embedIB bi)
+embedI IUnit = SUnit
+embedI IBool = SBool
+embedI IString = SString
+embedI IDesc = SDesc
+embedI IProg = error "TODO Embedding programs is not supported yet."
+embedI IType = SType
+embedI (IPi c bc) = SPi (embedC c) (embedCB bc)
+embedI (ISg c bc) = SSg (embedC c) (embedCB bc)
+
+embedI IDUnit = SDUnit
+embedI IDRec = SDRec
+embedI (IDSum c1 c2) = SDSum (embedC c1) (embedC c2)
+embedI (IDPi c bc) = SDPi (embedC c) (embedCB bc)
+embedI (IDSg c bc) = SDSg (embedC c) (embedCB bc)
+
+embedI (IDefs _) = error "TODO Embedding programs is not supported yet."
+embedI (IVar v) = SVar v
+embedI (IStrAppend c1 c2) = SStrAppend (embedC c1) (embedC c2)
+embedI (IStrEq c1 c2) = SStrEq (embedC c1) (embedC c2)
+embedI (IIf c i1 i2) = SIf (embedC c) (embedI i1) (embedI i2)
+embedI (ICaseBool bc c1 c2 c3) = SCaseBool (embedCB bc) (embedC c1) (embedC c2) (embedC c3)
+embedI (IProj1 i) = SProj1 (embedI i)
+embedI (IProj2 i) = SProj2 (embedI i)
+embedI (IApp i c) = SApp (embedI i) (embedC c)
+embedI (IAnn c1 c2) = SAnn (embedC c1) (embedC c2)
+
+embedC :: Check -> Syntax
+embedC (CPair c1 c2) = SPair (embedC c1) (embedC c2)
+embedC (CLam bc) = SLam (embedCB bc)
+embedC (Infer i) = embedI i
+
+----------------------------------------------------------------------
+
+embedCB :: Bound Check -> Bound Syntax
+embedCB (Bound (l , c)) = Bound (l , embedC c)
+
+embedIB :: Bound Infer -> Bound Syntax
+embedIB (Bound (l , i)) = Bound (l , embedI i)
+
+----------------------------------------------------------------------
