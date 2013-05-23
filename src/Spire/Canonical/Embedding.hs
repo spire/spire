75fdaee9f7a2162adfb24c5caa3d860170c58a7b
Nathan Collins <nathan.collins@gmail.com>
2013-05-22 17:57:36 -0700

    [minor] Thread identifier list through canonical embedders.
    This commit does not introduce any changes in functionality, but does
    change almost every line in the file. So, I factored it out from the
    actual bug fix (capture avoidance).  Another option here was a reader
    monad, but this version, with the explicit threading, is much easier
    to effect using `M-x replace-regexp` :P

--- src/Spire/Canonical/Embedding.hs
+++ src/Spire/Canonical/Embedding.hs
@@ -1,53 +1,64 @@
-module Spire.Canonical.Embedding where
+module Spire.Canonical.Embedding (embedV , embedN , embedVC) where
 import Spire.Expression.Types
 import Spire.Canonical.Types
 
 ----------------------------------------------------------------------
 
 embedV :: Val -> Infer
-embedV VUnit = IUnit
-embedV VBool = IBool
-embedV VString = IString
-embedV VDesc = IDesc
-embedV VProg = IProg
-embedV VType = IType
-embedV VTT = ITT
-embedV VTrue = ITrue
-embedV VFalse = IFalse
-embedV (VQuotes str) = IQuotes str
-
-embedV VDUnit = IDUnit
-embedV VDRec = IDRec
-embedV (VDSum d e) = IDSum (embedVC d) (embedVC e)
-embedV (VDPi aT fD) = IDPi (embedVC aT) (embedVBC fD)
-embedV (VDSg aT fD) = IDSg (embedVC aT) (embedVBC fD)
-
-embedV (VPi aT bT) = IPi (embedVC aT) (embedVBC bT)
-embedV (VSg aT bT) = ISg (embedVC aT) (embedVBC bT)
-embedV (VFix d) = IFix (embedVC d)
-embedV (VLam aT b) = ILamAnn (embedVC aT) (embedVB b)
-embedV (VPair aT bT a b) = IAnn
-  (CPair (embedVC a) (embedVC b))
-  (Infer (ISg (embedVC aT) (embedVBC bT)))
-embedV (VIn d a) = IAnn
-  (CIn (embedVC a))
-  (Infer (IFix (embedVC d)))
-embedV (Neut n) = embedN n
-embedV (VDefs _) = error "TODO Embedding programs is not supported yet."
+embedV = embedV' []
 
 embedN :: Neut -> Infer
-embedN (NVar (NomVar (l , _))) = IVar l
-embedN (NStrAppendL s1 s2) = IStrAppend (embedNC s1) (embedVC s2)
-embedN (NStrAppendR s1 s2) = IStrAppend (embedVC s1) (embedNC s2)
-embedN (NStrEqL s1 s2) = IStrEq (embedNC s1) (embedVC s2)
-embedN (NStrEqR s1 s2) = IStrEq (embedVC s1) (embedNC s2)
-embedN (NIf b c1 c2) = IIf (embedNC b) (embedV c1) (embedV c2)
-embedN (NCaseBool pT pt pf b) =
-  ICaseBool (embedVBC pT) (embedVC pt) (embedVC pf) (embedNC b)
-embedN (NProj1 ab) = IProj1 (embedN ab)
-embedN (NProj2 ab) = IProj2 (embedN ab)
-embedN (NApp f a) = IApp (embedN f) (embedVC a)
-embedN (NDInterp d e) = IDInterp (embedNC d) (embedVC e)
+embedN = embedN' []
+
+embedVC :: Val -> Check
+embedVC = embedVC' []
+
+----------------------------------------------------------------------
+
+embedV' :: [Ident] -> Val -> Infer
+embedV' is VUnit = IUnit
+embedV' is VBool = IBool
+embedV' is VString = IString
+embedV' is VDesc = IDesc
+embedV' is VProg = IProg
+embedV' is VType = IType
+embedV' is VTT = ITT
+embedV' is VTrue = ITrue
+embedV' is VFalse = IFalse
+embedV' is (VQuotes str) = IQuotes str
+
+embedV' is VDUnit = IDUnit
+embedV' is VDRec = IDRec
+embedV' is (VDSum d e) = IDSum (embedVC' is d) (embedVC' is e)
+embedV' is (VDPi aT fD) = IDPi (embedVC' is aT) (embedVBC' is fD)
+embedV' is (VDSg aT fD) = IDSg (embedVC' is aT) (embedVBC' is fD)
+
+embedV' is (VPi aT bT) = IPi (embedVC' is aT) (embedVBC' is bT)
+embedV' is (VSg aT bT) = ISg (embedVC' is aT) (embedVBC' is bT)
+embedV' is (VFix d) = IFix (embedVC' is d)
+embedV' is (VLam aT b) = ILamAnn (embedVC' is aT) (embedVB' is b)
+embedV' is (VPair aT bT a b) = IAnn
+  (CPair (embedVC' is a) (embedVC' is b))
+  (Infer (ISg (embedVC' is aT) (embedVBC' is bT)))
+embedV' is (VIn d a) = IAnn
+  (CIn (embedVC' is a))
+  (Infer (IFix (embedVC' is d)))
+embedV' is (Neut n) = embedN' is n
+embedV' is (VDefs _) = error "TODO Embedding' Is programs is not supported yet."
+
+embedN' :: [Ident] -> Neut -> Infer
+embedN' is (NVar (NomVar (l , _))) = IVar l -- XXX
+embedN' is (NStrAppendL s1 s2) = IStrAppend (embedNC' is s1) (embedVC' is s2)
+embedN' is (NStrAppendR s1 s2) = IStrAppend (embedVC' is s1) (embedNC' is s2)
+embedN' is (NStrEqL s1 s2) = IStrEq (embedNC' is s1) (embedVC' is s2)
+embedN' is (NStrEqR s1 s2) = IStrEq (embedVC' is s1) (embedNC' is s2)
+embedN' is (NIf b c1 c2) = IIf (embedNC' is b) (embedV' is c1) (embedV' is c2)
+embedN' is (NCaseBool pT pt pf b) =
+  ICaseBool (embedVBC' is pT) (embedVC' is pt) (embedVC' is pf) (embedNC' is b)
+embedN' is (NProj1 ab) = IProj1 (embedN' is ab)
+embedN' is (NProj2 ab) = IProj2 (embedN' is ab)
+embedN' is (NApp f a) = IApp (embedN' is f) (embedVC' is a)
+embedN' is (NDInterp d e) = IDInterp (embedNC' is d) (embedVC' is e)
 
 ----------------------------------------------------------------------
 
@@ -56,22 +67,22 @@ inferToCheck (IAnn a' _) = a'
 inferToCheck (ILamAnn _ (Bound (l , a'))) = CLam $ Bound (l , Infer a')
 inferToCheck a' = Infer a'
 
-embedVB :: Bound Val -> Bound Infer
-embedVB (Bound (l , a)) = Bound (l , embedV a)
+embedVB' :: [Ident] -> Bound Val -> Bound Infer
+embedVB' is (Bound (l , a)) = Bound (l , embedV' is a)
 
-embedNB :: Bound Neut -> Bound Infer
-embedNB (Bound (l , a)) = Bound (l , embedN a)
+embedNB' :: [Ident] -> Bound Neut -> Bound Infer
+embedNB' is (Bound (l , a)) = Bound (l , embedN' is a)
 
-embedVC :: Val -> Check
-embedVC =  inferToCheck . embedV
+embedVC' :: [Ident] -> Val -> Check
+embedVC' is =  inferToCheck . embedV' is
 
-embedNC :: Neut -> Check
-embedNC = inferToCheck . embedN
+embedNC' :: [Ident] -> Neut -> Check
+embedNC' is = inferToCheck . embedN' is
 
-embedVBC :: Bound Val -> Bound Check
-embedVBC (Bound (l , a)) = Bound (l , embedVC a)
+embedVBC' :: [Ident] -> Bound Val -> Bound Check
+embedVBC' is (Bound (l , a)) = Bound (l , embedVC' is a)
 
-embedNBC :: Bound Neut -> Bound Check
-embedNBC (Bound (l , a)) = Bound (l , embedNC a)
+embedNBC' :: [Ident] -> Bound Neut -> Bound Check
+embedNBC' is (Bound (l , a)) = Bound (l , embedNC' is a)
 
 ----------------------------------------------------------------------
