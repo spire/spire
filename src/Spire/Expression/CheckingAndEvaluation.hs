281e5d26a1e55f48ce89dca89b7a8f4b4e33d0d5
Nathan Collins <nathan.collins@gmail.com>
2013-06-06 14:45:39 -0700

    Put type checker context in a monad.
    There shouldn't be any functionality changes in this commit ...
    
    Warning: I'm using some ML stle rebinding (that I expected would not
    work) to bind a field value to it's name (similar to -XNamedFieldPuns,
    but in a 'Reader', I guess).  E.g., I use
    
      ctx <- asks ctx
    
    to store the 'ctx' field of the 'CheckR' reader state in the 'ctx'
    variable.  I.e., the 'ctx' on each side refer to different things.
    Maybe this is confusing?

--- src/Spire/Expression/CheckingAndEvaluation.hs
+++ src/Spire/Expression/CheckingAndEvaluation.hs
@@ -5,23 +5,36 @@ import Spire.Canonical.HereditarySubstitution
 import Spire.Expression.Types
 import Spire.Surface.PrettyPrinting
 import Control.Monad.Error
+import Control.Monad.Reader
 import Data.List
 
 ----------------------------------------------------------------------
 
-check :: Ctx -> Check -> Type -> Result Val
-check ctx (CLam b) (VPi aT bT) = do
-  b' <- checkExtend2 aT ctx b bT
+data CheckR = CheckR { ctx :: Ctx }
+type CheckM = ReaderT CheckR Result
+
+extendCtx :: Ident -> Type -> CheckM a -> CheckM a
+extendCtx l tp = local (\r -> r { ctx = (l , tp) : ctx r })
+
+run :: CheckM a -> CheckR -> Result a
+run = runReaderT
+
+----------------------------------------------------------------------
+
+check :: Check -> Type -> CheckM Val
+check (CLam b) (VPi aT bT) = do
+  b' <- checkExtend2 aT b bT
   return $ VLam aT b'
-check ctx (CPair a b) (VSg aT bT) = do
-  a' <- check ctx a aT
-  b' <- check ctx b (sub a' bT)
+check (CPair a b) (VSg aT bT) = do
+  a' <- check a aT
+  b' <- check b (sub a' bT)
   return $ VPair aT bT a' b'
-check ctx (CIn a) (VFix d) = do
-  a' <- check ctx a (evalDInterp d (VFix d))
+check (CIn a) (VFix d) = do
+  a' <- check a (evalDInterp d (VFix d))
   return $ VIn d a'
-check ctx (Infer a) bT = do
-  (a' , aT) <- infer ctx a
+check (Infer a) bT = do
+  ctx <- asks ctx
+  (a' , aT) <- infer a
   unless (aT == bT) $ throwError $
     "Ill-typed!\n" ++
     "Expected type:\n" ++ prettyPrintError bT ++
@@ -29,109 +42,110 @@ check ctx (Infer a) bT = do
     "\n\nContext:\n" ++ prettyPrintError ctx ++
     "\n\nUnevaluated value:\n" ++ prettyPrintError a
   return $ a'
-check ctx a aT = throwError "Ill-typed!"
+check a aT = throwError "Ill-typed!"
 
-infer :: Ctx -> Infer -> Result (Val , Type)
-infer ctx ITT     = return (VTT , VUnit)
-infer ctx ITrue   = return (VTrue , VBool)
-infer ctx IFalse  = return (VFalse , VBool)
-infer ctx IUnit   = return (VUnit , VType)
-infer ctx IBool   = return (VBool , VType)
-infer ctx IString = return (VString , VType)
-infer ctx IDesc   = return (VDesc , VType)
-infer ctx IProg   = return (VProg , VType)
-infer ctx IType   = return (VType , VType)
+infer :: Infer -> CheckM (Val , Type)
+infer ITT     = return (VTT , VUnit)
+infer ITrue   = return (VTrue , VBool)
+infer IFalse  = return (VFalse , VBool)
+infer IUnit   = return (VUnit , VType)
+infer IBool   = return (VBool , VType)
+infer IString = return (VString , VType)
+infer IDesc   = return (VDesc , VType)
+infer IProg   = return (VProg , VType)
+infer IType   = return (VType , VType)
 
-infer ctx IDUnit  = return (VDUnit , VDesc)
-infer ctx IDRec   = return (VDRec , VDesc)
-infer ctx (IDSum d e) = do
-  d' <- check ctx d VDesc
-  e' <- check ctx e VDesc
+infer IDUnit  = return (VDUnit , VDesc)
+infer IDRec   = return (VDRec , VDesc)
+infer (IDSum d e) = do
+  d' <- check d VDesc
+  e' <- check e VDesc
   return (VDSum d' e' , VDesc)
-infer ctx (IDPi aT fD) = do
-  aT' <- check ctx aT VType
-  fD' <- checkExtend aT' ctx fD VDesc
+infer (IDPi aT fD) = do
+  aT' <- check aT VType
+  fD' <- checkExtend aT' fD VDesc
   return (VDPi aT' fD' , VDesc)
-infer ctx (IDSg aT fD) = do
-  aT' <- check ctx aT VType
-  fD' <- checkExtend aT' ctx fD VDesc
+infer (IDSg aT fD) = do
+  aT' <- check aT VType
+  fD' <- checkExtend aT' fD VDesc
   return (VDSg aT' fD' , VDesc)
 
-infer ctx (IQuotes str) =
+infer (IQuotes str) =
   return (VQuotes str , VString)
-infer ctx (ILamAnn aT b) = do
-  aT'       <- check ctx aT VType
-  (b' , bT) <- inferExtend aT' ctx b
+infer (ILamAnn aT b) = do
+  aT'       <- check aT VType
+  (b' , bT) <- inferExtend aT' b
   return (VLam aT' b' , VPi aT' bT)
-infer ctx (IPi aT bT) = do
-  aT' <- check ctx aT VType
-  bT' <- checkExtend aT' ctx bT VType
+infer (IPi aT bT) = do
+  aT' <- check aT VType
+  bT' <- checkExtend aT' bT VType
   return (VPi aT' bT' , VType)
-infer ctx (ISg aT bT) = do
-  aT' <- check ctx aT VType
-  bT' <- checkExtend aT' ctx bT VType
+infer (ISg aT bT) = do
+  aT' <- check aT VType
+  bT' <- checkExtend aT' bT VType
   return (VSg aT' bT' , VType)
-infer ctx (IFix d) = do
-  d' <- check ctx d VDesc
+infer (IFix d) = do
+  d' <- check d VDesc
   return (VFix d' , VType)
-infer ctx (IDefs as) = do
-  as' <- checkDefs [] ctx as
+infer (IDefs as) = do
+  as' <- checkDefs [] as
   let as'' = map (\(_ , a , aT) -> (a , aT))  as'
   return (VDefs as'' , VProg)
-infer ctx (IStrAppend s1 s2) = do
- s1' <- check ctx s1 VString
- s2' <- check ctx s2 VString
+infer (IStrAppend s1 s2) = do
+ s1' <- check s1 VString
+ s2' <- check s2 VString
  return (evalStrAppend s1' s2' , VString)
-infer ctx (IStrEq s1 s2) = do
- s1' <- check ctx s1 VString
- s2' <- check ctx s2 VString
+infer (IStrEq s1 s2) = do
+ s1' <- check s1 VString
+ s2' <- check s2 VString
  return (evalStrEq s1' s2' , VBool)
-infer ctx (IIf b c1 c2) = do
-  b'          <- check ctx b VBool
-  (c1' , cT1) <- infer ctx c1
-  (c2' , cT2) <- infer ctx c2
+infer (IIf b c1 c2) = do
+  b'          <- check b VBool
+  (c1' , cT1) <- infer c1
+  (c2' , cT2) <- infer c2
   unless (cT1 == cT2) $ throwError $
     "Ill-typed, conditional branches have different types!\n" ++
     "First branch:\n" ++ prettyPrintError cT1 ++
     "\nSecond branch:\n" ++ prettyPrintError cT2
   return (evalIf b' c1' c2' , cT1)
-infer ctx (ICaseBool pT pt pf b) = do
-  pT' <- checkExtend VBool ctx pT VType
-  pt' <- check ctx pt (sub VTrue pT')
-  pf' <- check ctx pf (sub VFalse pT')
-  b'  <- check ctx b VBool
+infer (ICaseBool pT pt pf b) = do
+  pT' <- checkExtend VBool pT VType
+  pt' <- check pt (sub VTrue pT')
+  pf' <- check pf (sub VFalse pT')
+  b'  <- check b VBool
   return (evalCaseBool pT' pt' pf' b' , sub b' pT')
-infer ctx (IDInterp d e) = do
-  d' <- check ctx d VDesc
-  e' <- check ctx e VType
+infer (IDInterp d e) = do
+  d' <- check d VDesc
+  e' <- check e VType
   return (evalDInterp d' e' , VType)
-infer ctx (IProj1 ab) = do
-  (ab' , abT) <- infer ctx ab
+infer (IProj1 ab) = do
+  (ab' , abT) <- infer ab
   case abT of
     VSg aT bT -> return (evalProj1 ab' , aT)
     _ -> throwError $
       "Ill-typed, projection of non-pair!\n" ++
       "Projected value:\n" ++ prettyPrintError ab ++
       "\nProjected type:\n" ++ prettyPrintError abT
-infer ctx (IProj2 ab) = do
-  (ab' , abT) <- infer ctx ab
+infer (IProj2 ab) = do
+  (ab' , abT) <- infer ab
   case abT of
     VSg aT bT -> return (evalProj2 ab' , sub (evalProj1 ab') bT)
     _ -> throwError $
       "Ill-typed, projection of non-pair!\n" ++
       "Projected value:\n" ++ prettyPrintError ab ++
       "\nProjected type:\n" ++ prettyPrintError abT
-infer ctx (IApp f a) = do
-  (f' , fT) <- infer ctx f
+infer (IApp f a) = do
+  (f' , fT) <- infer f
   case fT of
     VPi aT bT -> do
-      a' <- check ctx a aT
+      a' <- check a aT
       return (evalApp f' a' , sub a' bT)
     _ -> throwError $
       "Ill-typed, application of non-function!\n" ++
       "Applied value:\n"  ++ prettyPrintError f ++
       "\nApplied type:\n"  ++ prettyPrintError fT
-infer ctx (IVar l) =
+infer (IVar l) = do
+  ctx <- asks ctx
   -- XXX: this error check is dubious: the variable is looked up by
   -- name here, and by numeric index below.  These need not agree in
   -- the face of DeBruijn bugs ...
@@ -142,43 +156,44 @@ infer ctx (IVar l) =
       "\nCurrent context:\n" ++ show (map fst ctx)
     Just i ->
       return (Neut (NVar (NomVar (l , i))) , snd (ctx !! i))
-infer ctx (IAnn a aT) = do
-  aT' <- check ctx aT VType
-  a'  <- check ctx a aT'
+infer (IAnn a aT) = do
+  aT' <- check aT VType
+  a'  <- check a aT'
   return (a' , aT')
 
 ----------------------------------------------------------------------
 
-checkExtend2 :: Type -> Ctx -> Bound Check -> Bound Type -> Result (Bound Val)
-checkExtend2 aT ctx b (Bound (_ , bT)) = checkExtend aT ctx b bT
+checkExtend2 :: Type -> Bound Check -> Bound Type -> CheckM (Bound Val)
+checkExtend2 aT b (Bound (_ , bT)) = checkExtend aT b bT
 
-checkExtend :: Type -> Ctx -> Bound Check -> Type -> Result (Bound Val)
-checkExtend aT ctx (Bound (l , b)) bT = do
-  b' <- check ((l , aT) : ctx) b bT
+checkExtend :: Type -> Bound Check -> Type -> CheckM (Bound Val)
+checkExtend aT (Bound (l , b)) bT = do
+  b' <- extendCtx l aT $ check b bT
   return $ Bound (l , b')
 
-inferExtend :: Type -> Ctx -> Bound Infer -> Result (Bound Val , Bound Type)
-inferExtend aT ctx (Bound (l , b)) = do
-  (b' , bT) <- infer ((l , aT) : ctx) b
+inferExtend :: Type -> Bound Infer -> CheckM (Bound Val , Bound Type)
+inferExtend aT (Bound (l , b)) = do
+  (b' , bT) <- extendCtx l aT $ infer b
   return (Bound (l , b') , Bound (l , bT))
 
-checkDefs :: [Val] -> Ctx -> [Def] -> Result [(Ident , Val , Type)]
-checkDefs x ctx [] = return []
-checkDefs xs ctx ((l , a , aT) : as) = do
-  aT' <- return . foldSub xs =<< check ctx aT VType
-  a' <- return . foldSub xs =<< check ctx a aT'
-  as' <- checkDefs (a' : xs) ((l , aT') : ctx) as
+checkDefs :: [Val] -> [Def] -> CheckM [(Ident , Val , Type)]
+checkDefs x [] = return []
+checkDefs xs ((l , a , aT) : as) = do
+  aT' <- return . foldSub xs =<< check aT VType
+  a' <- return . foldSub xs =<< check a aT'
+  as' <- extendCtx l aT' $ checkDefs (a' : xs) as
   return ((l , a' , aT') : as')
 
 checkDefsStable :: [Def] -> Result [Def]
-checkDefsStable as = do
-  as' <- checkDefs [] [] as
-  let bs = embedDefs as'
-  bs' <- checkDefs [] [] bs
-  unless (as' == bs') $ throwError $
-    "Embedding is unstable!"
-  return bs
+checkDefsStable as = run (checkDefsStableM as) (CheckR { ctx = [] })
   where
+  checkDefsStableM as = do
+    as' <- checkDefs [] as
+    let bs = embedDefs as'
+    bs' <- checkDefs [] bs
+    unless (as' == bs') $ throwError $
+      "Embedding is unstable!"
+    return bs
   embedDefs = map (\(l , a , aT) -> (l , embedVC a , embedVC aT))
 
 ----------------------------------------------------------------------
