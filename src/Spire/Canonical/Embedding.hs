50afaa0206827ee85b4fc2af69bcb116fe436da6
Nathan Collins <nathan.collins@gmail.com>
2013-06-05 17:56:35 -0700

    Make embedders use an error monad.
    It is problematic when embedders fail while printing error messages.
    So, I put the embedders in an error monad.  However, there aren't
    really any "important" errors in the embedders: I thought the one for
    free variables was important (the 'NVar' case of 'embedN''), because
    it could be due to DeBruijn errors, but then I realized it also
    happens for (DeBruijn correct) open terms :P
    
    So, what we really need to do is handle the free variables correctly.
    The approaches that come immediately to mind are:
    
    1. take the free variables into account when alpha renaming bound
    variables.
    
    2. return a substitution, in addition to the embedded term.
    
    It seems (1) is much easier to work worth, so I'll try that ... since
    I'm already in a monad, it's easy to add another 'Reader' field, for
    the free variables.
    
    For the hypothetical problem of pretty printing itself, when DeBruijn
    errors mess up the embedding, a better solution is probably the many
    pretty printers I had before, which didn't use embedding: this way we
    would be able to pretty print while debugging DeBruijn errors.
    
    More details about the problem we need to address: When going from
    DeBruijn to names, the canonical embedders ('embedV', 'embedN') must
    sometimes create fresh variables ('extendIdents').  However, this
    freshness check is not done w.r.t. the free variables in the original
    term, but only w.r.t. the enclosing binders.
    
    E.g., consider the term, which has a free occurrence of x' (x'2):
    
      \x. \x. (x0 , x'2)
    
    When going from DeBruijn to names, we freshen (using 'extendIdents')
    to:
    
      \x. \x'. (x'0 , <<free variable error>>)
    
    and so it would *not* be safe to instead replace the fresh variable
    with it's name, since it would be captured:
    
      \x. \x'. (x'0 , x'2)
    
    And actually, this example is overly complicated, e.g. we may have
    already captured a free variable (name) when (safely DeBruijn)
    substituting.  E.g., evaluting
    
      (\x. \y. x1) y0 ~> (\y. y1)
    
    captures the (name of the) free y.

--- src/Spire/Canonical/Embedding.hs
+++ src/Spire/Canonical/Embedding.hs
@@ -1,73 +1,83 @@
 module Spire.Canonical.Embedding (embedV , embedN , embedVC) where
 import Spire.Expression.Types
 import Spire.Canonical.Types
+import Control.Applicative
+import Control.Monad.Error
+import Control.Monad.Reader
 
 ----------------------------------------------------------------------
 
-embedV :: Val -> Infer
-embedV = embedV' []
+embedV :: Val -> Either String Infer
+embedV = run embedV'
 
-embedN :: Neut -> Infer
-embedN = embedN' []
+embedN :: Neut -> Either String Infer
+embedN = run embedN'
 
-embedVC :: Val -> Check
-embedVC = embedVC' []
+embedVC :: Val -> Either String Check
+embedVC = run embedVC'
+
+run :: (a -> EmbedM b) -> a -> Either String b
+run f x = runReader (runErrorT $ f x) []
 
 ----------------------------------------------------------------------
 
-embedV' :: [Ident] -> Val -> Infer
-embedV' is VUnit = IUnit
-embedV' is VBool = IBool
-embedV' is VString = IString
-embedV' is VDesc = IDesc
-embedV' is VProg = IProg
-embedV' is VType = IType
-embedV' is VTT = ITT
-embedV' is VTrue = ITrue
-embedV' is VFalse = IFalse
-embedV' is (VQuotes str) = IQuotes str
+type EmbedM a = ErrorT String (Reader [Ident]) a
 
-embedV' is VDUnit = IDUnit
-embedV' is VDRec = IDRec
-embedV' is (VDSum d e) = IDSum (embedVC' is d) (embedVC' is e)
-embedV' is (VDPi aT fD) = IDPi (embedVC' is aT) (embedVBC' is fD)
-embedV' is (VDSg aT fD) = IDSg (embedVC' is aT) (embedVBC' is fD)
+embedV' :: Val -> EmbedM Infer
+embedV' VUnit = return IUnit
+embedV' VBool = return IBool
+embedV' VString = return IString
+embedV' VDesc = return IDesc
+embedV' VProg = return IProg
+embedV' VType = return IType
+embedV' VTT = return ITT
+embedV' VTrue = return ITrue
+embedV' VFalse = return IFalse
+embedV' (VQuotes str) = return $ IQuotes str
 
-embedV' is (VPi aT bT) = IPi (embedVC' is aT) (embedVBC' is bT)
-embedV' is (VSg aT bT) = ISg (embedVC' is aT) (embedVBC' is bT)
-embedV' is (VFix d) = IFix (embedVC' is d)
-embedV' is (VLam aT b) = ILamAnn (embedVC' is aT) (embedVB' is b)
-embedV' is (VPair aT bT a b) = IAnn
-  (CPair (embedVC' is a) (embedVC' is b))
-  (Infer (ISg (embedVC' is aT) (embedVBC' is bT)))
-embedV' is (VIn d a) = IAnn
-  (CIn (embedVC' is a))
-  (Infer (IFix (embedVC' is d)))
-embedV' is (Neut n) = embedN' is n
-embedV' is (VDefs _) = error "TODO Embedding' Is programs is not supported yet."
+embedV' VDUnit = return IDUnit
+embedV' VDRec = return IDRec
+embedV' (VDSum d e) = IDSum <$> embedVC' d <*> embedVC' e
+embedV' (VDPi aT fD) = IDPi <$> embedVC' aT <*> embedVBC' fD
+embedV' (VDSg aT fD) = IDSg <$> embedVC' aT <*> embedVBC' fD
 
-embedN' :: [Ident] -> Neut -> Infer
-embedN' is (NVar n@(NomVar (_ , k))) =
+embedV' (VPi aT bT) = IPi <$> embedVC' aT <*> embedVBC' bT
+embedV' (VSg aT bT) = ISg <$> embedVC' aT <*> embedVBC' bT
+embedV' (VFix d) = IFix <$> embedVC' d
+embedV' (VLam aT b) = ILamAnn <$> embedVC' aT <*> embedVB' b
+embedV' (VPair aT bT a b) = IAnn <$>
+  (CPair <$> embedVC' a <*> embedVC' b) <*>
+  (Infer <$> (ISg <$> embedVC' aT <*> embedVBC' bT))
+embedV' (VIn d a) = IAnn <$>
+  (CIn <$> embedVC' a) <*>
+  (Infer <$> (IFix <$> embedVC' d))
+embedV' (Neut n) = embedN' n
+embedV' (VDefs _) = throwError
+  "TODO Embedding' Is programs is not supported yet."
+
+embedN' :: Neut -> EmbedM Infer
+embedN' (NVar n@(NomVar (_ , k))) = do
+  is <- ask
   if k >= length is
   -- XXX: this is more evidence that 'embed*' should be in a monad:
   -- then we could raise monadic errors, and thread the binders using
   -- a reader (in other words: once you give up and go into a monad,
   -- it's easier to do other monadic things).
-  then error $
+  then throwError $
          "Internal error: DeBruijn variable unbound in 'embedN':\n" ++
          "var = " ++ show n ++ ", binders = " ++ show is
-  else IVar (is !! k)
-embedN' is (NStrAppendL s1 s2) = IStrAppend (embedNC' is s1) (embedVC' is s2)
-embedN' is (NStrAppendR s1 s2) = IStrAppend (embedVC' is s1) (embedNC' is s2)
-embedN' is (NStrEqL s1 s2) = IStrEq (embedNC' is s1) (embedVC' is s2)
-embedN' is (NStrEqR s1 s2) = IStrEq (embedVC' is s1) (embedNC' is s2)
-embedN' is (NIf b c1 c2) = IIf (embedNC' is b) (embedV' is c1) (embedV' is c2)
-embedN' is (NCaseBool pT pt pf b) =
-  ICaseBool (embedVBC' is pT) (embedVC' is pt) (embedVC' is pf) (embedNC' is b)
-embedN' is (NProj1 ab) = IProj1 (embedN' is ab)
-embedN' is (NProj2 ab) = IProj2 (embedN' is ab)
-embedN' is (NApp f a) = IApp (embedN' is f) (embedVC' is a)
-embedN' is (NDInterp d e) = IDInterp (embedNC' is d) (embedVC' is e)
+  else return $ IVar (is !! k)
+embedN' (NStrAppendL s1 s2) = IStrAppend <$> embedNC' s1 <*> embedVC' s2
+embedN' (NStrAppendR s1 s2) = IStrAppend <$> embedVC' s1 <*> embedNC' s2
+embedN' (NStrEqL s1 s2) = IStrEq <$> embedNC' s1 <*> embedVC' s2
+embedN' (NStrEqR s1 s2) = IStrEq <$> embedVC' s1 <*> embedNC' s2
+embedN' (NIf b c1 c2) = IIf <$> embedNC' b <*> embedV' c1 <*> embedV' c2
+embedN' (NCaseBool pT pt pf b) =
+  ICaseBool <$> embedVBC' pT <*> embedVC' pt <*> embedVC' pf <*> embedNC' b
+embedN' (NProj1 ab) = IProj1 <$> embedN' ab
+embedN' (NProj2 ab) = IProj2 <$> embedN' ab
+embedN' (NApp f a) = IApp <$> embedN' f <*> embedVC' a
+embedN' (NDInterp d e) = IDInterp <$> embedNC' d <*> embedVC' e
 
 ----------------------------------------------------------------------
 
@@ -76,31 +86,30 @@ inferToCheck (IAnn a' _) = a'
 inferToCheck (ILamAnn _ (Bound (l , a'))) = CLam $ Bound (l , Infer a')
 inferToCheck a' = Infer a'
 
-embedVB' :: [Ident] -> Bound Val -> Bound Infer
-embedVB' is (Bound (l , a)) = Bound (l' , embedV' is' a)
-  where
-  (l' , is') = extendIdents l is
+embedVB' :: Bound Val -> EmbedM (Bound Infer)
+embedVB' = liftEmbedBound embedV'
 
-embedNB' :: [Ident] -> Bound Neut -> Bound Infer
-embedNB' is (Bound (l , a)) = Bound (l' , embedN' is' a)
-  where
-  (l' , is') = extendIdents l is
+embedNB' :: Bound Neut -> EmbedM (Bound Infer)
+embedNB' = liftEmbedBound embedN'
 
-embedVC' :: [Ident] -> Val -> Check
-embedVC' is =  inferToCheck . embedV' is
+embedVC' :: Val -> EmbedM Check
+embedVC' v =  inferToCheck <$> embedV' v
 
-embedNC' :: [Ident] -> Neut -> Check
-embedNC' is = inferToCheck . embedN' is
+embedNC' :: Neut -> EmbedM Check
+embedNC' n = inferToCheck <$> embedN' n
 
-embedVBC' :: [Ident] -> Bound Val -> Bound Check
-embedVBC' is (Bound (l , a)) = Bound (l' , embedVC' is' a)
-  where
-  (l' , is') = extendIdents l is
+embedVBC' :: Bound Val -> EmbedM (Bound Check)
+embedVBC' = liftEmbedBound embedVC'
 
-embedNBC' :: [Ident] -> Bound Neut -> Bound Check
-embedNBC' is (Bound (l , a)) = Bound (l' , embedNC' is' a)
-  where
-  (l' , is') = extendIdents l is
+embedNBC' :: Bound Neut -> EmbedM (Bound Check)
+embedNBC' = liftEmbedBound embedNC'
+
+-- Lift an embedder for 'a's to an embedder for bound 'a's.
+liftEmbedBound :: (a -> EmbedM b) -> Bound a -> EmbedM (Bound b)
+liftEmbedBound embed (Bound (l , a)) = do
+  (l' , is') <- asks $ extendIdents l
+  a' <- local (const is') $ embed a
+  return $ Bound (l' , a')
 
 -- Add 'l' to 'is', freshening by appending primes if necessary.
 extendIdents :: Ident -> [Ident] -> (Ident , [Ident])
