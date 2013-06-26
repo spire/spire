2fdc5fb038f1822584c629cf9eda036953bc3369
Nathan Collins <nathan.collins@gmail.com>
2013-06-26 15:18:11 -0700

    CP: Indexed Exp and index constraints.

--- proposal/examples/type-changing-evaluator/TypeChanging.hs
+++ proposal/examples/type-changing-evaluator/TypeChanging.hs
@@ -9,33 +9,44 @@ import Data.Generics
 ----------------------------------------------------------------
 -- Lambda calc with DeBruijn vars.
 
--- data Exp (i :: I) where
-data Exp (i :: *) where
+data Exp (i :: I) where
   Var  :: VarI i     => Int -> Exp i
   Lam  :: LamI i j   => Binder (Exp i) -> Exp j
   (:@) :: AppI i j k => Exp i -> Exp j -> Exp k
+  Lift :: LiftI i j  => Exp i -> Exp j
 
 newtype Binder a = Binder a
   deriving (Eq , Show , Data , Typeable)
 
-deriving instance Eq (Exp i)
+-- deriving instance Eq (Exp i)
 deriving instance Show (Exp i)
-deriving instance Data (Exp i)
--- Not allowed to derive typeable for 'i' of kind other than '*'.
-deriving instance Typeable1 Exp
+-- deriving instance Data (Exp i)
+-- -- Not allowed to derive typeable for 'i' of kind other than '*'.
+-- deriving instance Typeable1 Exp
 
--- data I = E | M | R
-data E
-data M
-data R
+----------------------------------------------------------------
+-- Index constraints.
 
--- class VarI (i :: I)
-class VarI i
+data I = E | M | R
+-- data E
+-- data M
+-- data R
+
+class VarI (i :: I)
+-- class VarI i
 instance VarI E
 instance VarI R
 
--- class AppI (i :: I) (j :: I) (k :: I)
-class AppI i j k
+class LamI (i :: I) (j :: I)
+-- class LamI i j
+instance LamI E E
+instance LamI M M
 
--- class LamI (i :: I) (j :: I)
-class LamI i j
+class AppI (i :: I) (j :: I) (k :: I)
+-- class AppI i j k
+instance AppI E E E
+instance AppI R M R
+
+class LiftI (i :: I) (j :: I)
+instance LiftI R M
+
