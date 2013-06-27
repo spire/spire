f60018b916296103016e5ddaa2933faa34f29148
Nathan Collins <nathan.collins@gmail.com>
2013-06-26 17:54:00 -0700

    CP: add weakening and substitution that don't type check.
    GHC is failing to use the fundeps ... I'm not sure why.
    Will try type functions next.

--- proposal/examples/type-changing-evaluator/TypeChanging.hs
+++ proposal/examples/type-changing-evaluator/TypeChanging.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable
+  , FunctionalDependencies
+  , RankNTypes
   , StandaloneDeriving
   , GADTs
   , DataKinds
@@ -37,16 +39,55 @@ class VarI (i :: I)
 instance VarI E
 instance VarI R
 
-class LamI (i :: I) (j :: I)
+class LamI (i :: I) (j :: I) | j -> i
 -- class LamI i j
 instance LamI E E
 instance LamI M M
 
-class AppI (i :: I) (j :: I) (k :: I)
+class AppI (i :: I) (j :: I) (k :: I) | k -> i j
 -- class AppI i j k
 instance AppI E E E
 instance AppI R M R
 
-class LiftI (i :: I) (j :: I)
+class LiftI (i :: I) (j :: I) | j -> i
 instance LiftI R M
 
+----------------------------------------------------------------
+-- Non generic, non monadic operations.
+
+-- Increment all free variables:
+-- If G |- e:B then G,A |- weaken e:B.
+weaken1 :: Exp e -> Exp e
+weaken1 = w 0
+  where
+  w :: Int -> Exp e -> Exp e
+  w i (Var j)          = wVar i j
+  w i (e1 :@ e2)       = w i e1 :@ w i e2
+  w i (Lam (Binder e)) = Lam (Binder (w (succ i) e))
+  w i (Lift e)         = Lift (w i e)
+
+wVar :: VarI e => Int -> Int -> Exp e
+wVar i j | i <= j    = Var (succ j)
+         | otherwise = Var j
+
+-- Substitute an expression for variable zero:
+-- If G |- e1:A and G,A |- e2:B then G |- substitute e1 e2:B.
+substitute1 :: (forall e. Exp e -> Exp e) -> Exp M -> Exp M -> Exp M
+substitute1 weaken = sM 0
+  where
+  sM :: Int -> Exp M -> Exp M -> Exp M
+  sM i e0 (Lam (Binder e)) = Lam (Binder (sM (succ i) (weaken e0) e))
+  sM i e0 (Lift e)         = sR i e0 e
+
+  sR :: Int -> Exp M -> Exp R -> Exp M
+  sR i e0 (Var j)          = sVar i e0 j
+  sR i e0 (e1 :@ e2)       = sR i e0 e1 `app` sM i e0 e2
+
+  app :: Exp M -> Exp M -> Exp M
+  app (Lift e) e' = e :@ e'
+  app (Lam e)  e' = substitute1 e' e
+
+sVar :: Int -> Exp M -> Int -> Exp M
+sVar i e0 j | i == j    = e0
+            | i <  j    = Lift $ Var (pred j)
+            | otherwise = Lift $ Var j
