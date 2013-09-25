475c329d7a2643515a67ce58649f3d4c7e5ca19a
Nathan Collins <nathan.collins@gmail.com>
2013-09-24 21:09:16 -0700

    Remove commented out code in Spire.Unbound.SubstM.
    Doing this in a separate commit in case someone wants to
    revert and add a 'substsM' implementation for some reason.

--- src/Spire/Unbound/SubstM.hs
+++ src/Spire/Unbound/SubstM.hs
@@ -7,15 +7,11 @@
   #-}
 
 module Spire.Unbound.SubstM where
--- import Data.List (find)
 import Generics.RepLib
 import Unbound.LocallyNameless.Types
 import Unbound.LocallyNameless.Alpha
 
 ----------------------------------------------------------------------
-
--- data SubstCoerceM m a b =
---   SubstCoerceM (Name b) (b -> Maybe (m a))
 
 type SubstMType m b a = Name b -> b -> a -> m a
 type SubstHookMType m b a = a -> Maybe (Name b -> b -> m a)
@@ -31,9 +27,6 @@ class (Monad m, Rep1 (SubstDM m b) a) => SubstM m b a where
   substHookM :: SubstHookMType m b a
   substHookM _ = Nothing
 
-  -- isVarM :: a -> Maybe (SubstCoerceM m a b)
-  -- isVarM _ = Nothing
-
   substM :: SubstMType m b a
   substM n u x | isFree n =
      case substHookM x of
@@ -41,33 +34,15 @@ class (Monad m, Rep1 (SubstDM m b) a) => SubstM m b a where
        Nothing -> substR1M rep1 n u x
   substM m _ _ = error $ "Cannot substitute for bound variable " ++ show m
 
-  -- XXX: Not sure how to write this now since we've essentially lost
-  -- the notion of base case in substitution.  I.e., the below code
-  -- seems to have independent base cases in mind (variables), but we
-  -- have no such base cases ...
-
-  -- substsM :: [(Name b, b)] -> a -> m a
-  -- substsM ss x
-  --   | all (isFree . fst) ss =
-  --       case isVarM x of 
-  --         Just (SubstCoerceM m f) ->
-  --           case find ((==m) . fst) ss of 
-  --               Just (_, u) -> maybe (return x) id (f u)
-  --               Nothing -> return x
-  --         Nothing -> substsR1M rep1 ss x
-  --   | otherwise =
-  --     error $ "Cannot substitute for bound variable in: " ++ show (map fst ss)
-
 ----------------------------------------------------------------------
 
 data SubstDM m b a = SubstDM {
   substHookDM :: SubstHookMType m b a ,
   substDM :: SubstMType m b a
-  -- substsDM :: [(Name b, b)] -> a -> m a
 }
 
 instance (SubstM m b a) => Sat (SubstDM m b a) where
-  dict = SubstDM substHookM substM --substsM
+  dict = SubstDM substHookM substM
 
 substDefaultM :: Monad m => Rep1 (SubstDM m b) a => SubstMType m b a
 substDefaultM = substR1M rep1
@@ -81,14 +56,6 @@ substR1M (Data1 _dt cons) = \ x y d ->
       let z = mapM_l (\ w -> substDM w x y) rec kids
       in return . to c =<< z
 substR1M _ = \ _ _ c -> return c
-
--- substsR1M :: Monad m => R1 (SubstDM m b) a -> [(Name b, b)] -> a -> m a
--- substsR1M (Data1 _dt cons) = \ s d ->
---   case (findCon cons d) of
---   Val c rec kids ->
---       let z = mapM_l (\ w -> substsDM w s) rec kids
---       in return . to c =<< z
--- substsR1M _ = \ _ c -> return c
 
 ----------------------------------------------------------------------
 
