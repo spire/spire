50cc5c8409761f2a89e873a3e97a4d2acdfb2b48
Nathan Collins <nathan.collins@gmail.com>
2013-09-24 20:43:31 -0700

    Fix substitution bug.
    There may be a fancier way to implement this, where the library still
    makes the recursive substitution calls, but this approach seems both
    flexible and light weight: in the cases where variables are
    encountered, or where post processing is needed, the user defines
    'substHookM' to return a hook function that takes over the
    substitution at that point.  In our code, the "variable" case and the
    "post processing" case are combined for 'VNeut'.
    
    Here's the instance:
    
      instance SubstM SpireM Value Value where
        substHookM (VNeut nm fs) = Just f
          where
          f nm' e = do
            fs' <- substM nm' e fs
            let head = if nm == nm' then e else vVar nm
            elims head fs'
        substHookM _ = Nothing
    
    One possible downside is that the parallel substitution 'substsM'
    seems hard to define now. But we weren't using that anyway.

--- src/Spire/Unbound/SubstM.hs
+++ src/Spire/Unbound/SubstM.hs
@@ -7,57 +7,74 @@
   #-}
 
 module Spire.Unbound.SubstM where
-import Data.List (find)
+-- import Data.List (find)
 import Generics.RepLib
 import Unbound.LocallyNameless.Types
 import Unbound.LocallyNameless.Alpha
 
 ----------------------------------------------------------------------
 
-data SubstCoerceM m a b =
-  SubstCoerceM (Name b) (b -> Maybe (m a))
+-- data SubstCoerceM m a b =
+--   SubstCoerceM (Name b) (b -> Maybe (m a))
 
+type SubstMType m b a = Name b -> b -> a -> m a
+type SubstHookMType m b a = a -> Maybe (Name b -> b -> m a)
+
+-- Substitute 'b's into 'a's in the monad 'm'.
+--
+-- The interface is 'substHookM': the user should define it in all
+-- cases where a variable is encountered or where the results of a
+-- regular substitution needs to be post processed (e.g. in hereditary
+-- substitution).
 class (Monad m, Rep1 (SubstDM m b) a) => SubstM m b a where
 
-  isVarM :: a -> Maybe (SubstCoerceM m a b)
-  isVarM _ = Nothing
+  substHookM :: SubstHookMType m b a
+  substHookM _ = Nothing
 
-  substM :: Name b -> b -> a -> m a
+  -- isVarM :: a -> Maybe (SubstCoerceM m a b)
+  -- isVarM _ = Nothing
+
+  substM :: SubstMType m b a
   substM n u x | isFree n =
-     case isVarM x of
-       Just (SubstCoerceM m f) -> if m == n then maybe (return x) id (f u) else return x
+     case substHookM x of
+       Just f -> f n u
        Nothing -> substR1M rep1 n u x
   substM m _ _ = error $ "Cannot substitute for bound variable " ++ show m
 
-  substsM :: [(Name b, b)] -> a -> m a
-  substsM ss x
-    | all (isFree . fst) ss =
-        case isVarM x of 
-          Just (SubstCoerceM m f) ->
-            case find ((==m) . fst) ss of 
-                Just (_, u) -> maybe (return x) id (f u)
-                Nothing -> return x
-          Nothing -> substsR1M rep1 ss x
-    | otherwise =
-      error $ "Cannot substitute for bound variable in: " ++ show (map fst ss)
+  -- XXX: Not sure how to write this now since we've essentially lost
+  -- the notion of base case in substitution.  I.e., the below code
+  -- seems to have independent base cases in mind (variables), but we
+  -- have no such base cases ...
+
+  -- substsM :: [(Name b, b)] -> a -> m a
+  -- substsM ss x
+  --   | all (isFree . fst) ss =
+  --       case isVarM x of 
+  --         Just (SubstCoerceM m f) ->
+  --           case find ((==m) . fst) ss of 
+  --               Just (_, u) -> maybe (return x) id (f u)
+  --               Nothing -> return x
+  --         Nothing -> substsR1M rep1 ss x
+  --   | otherwise =
+  --     error $ "Cannot substitute for bound variable in: " ++ show (map fst ss)
 
 ----------------------------------------------------------------------
 
 data SubstDM m b a = SubstDM {
-  isVarDM  :: a -> Maybe (SubstCoerceM m a b),
-  substDM  :: Name b -> b -> a -> m a,
-  substsDM :: [(Name b, b)] -> a -> m a
+  substHookDM :: SubstHookMType m b a ,
+  substDM :: SubstMType m b a
+  -- substsDM :: [(Name b, b)] -> a -> m a
 }
 
 instance (SubstM m b a) => Sat (SubstDM m b a) where
-  dict = SubstDM isVarM substM substsM
+  dict = SubstDM substHookM substM --substsM
 
-substDefaultM :: Monad m => Rep1 (SubstDM m b) a => Name b -> b -> a -> m a
+substDefaultM :: Monad m => Rep1 (SubstDM m b) a => SubstMType m b a
 substDefaultM = substR1M rep1
 
 ----------------------------------------------------------------------
 
-substR1M :: Monad m => R1 (SubstDM m b) a -> Name b -> b -> a -> m a
+substR1M :: Monad m => R1 (SubstDM m b) a -> SubstMType m b a
 substR1M (Data1 _dt cons) = \ x y d ->
   case (findCon cons d) of
   Val c rec kids ->
@@ -65,13 +82,13 @@ substR1M (Data1 _dt cons) = \ x y d ->
       in return . to c =<< z
 substR1M _ = \ _ _ c -> return c
 
-substsR1M :: Monad m => R1 (SubstDM m b) a -> [(Name b, b)] -> a -> m a
-substsR1M (Data1 _dt cons) = \ s d ->
-  case (findCon cons d) of
-  Val c rec kids ->
-      let z = mapM_l (\ w -> substsDM w s) rec kids
-      in return . to c =<< z
-substsR1M _ = \ _ c -> return c
+-- substsR1M :: Monad m => R1 (SubstDM m b) a -> [(Name b, b)] -> a -> m a
+-- substsR1M (Data1 _dt cons) = \ s d ->
+--   case (findCon cons d) of
+--   Val c rec kids ->
+--       let z = mapM_l (\ w -> substsDM w s) rec kids
+--       in return . to c =<< z
+-- substsR1M _ = \ _ c -> return c
 
 ----------------------------------------------------------------------
 
