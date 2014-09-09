7f01f637c7ecf9c9387135966b3eb659d5cb081c
Larry Diehl <larrytheliquid@gmail.com>
2014-09-09 13:44:16 -0700

    wip

--- mono/Expr.hs
+++ mono/Expr.hs
@@ -1,59 +1,77 @@
 {-# LANGUAGE TypeSynonymInstances
-           , GeneralizedNewtypeDeriving
+           , MultiParamTypeClasses
+           , FlexibleInstances
   #-}
 
 
 module Expr where
 import Control.Monad
-import Control.Monad.State
-import Name
+import Control.Monad.State hiding ( lift )
 
 ----------------------------------------------------------------------
+
+data Name a = Fr String | Bn Integer
+  deriving (Show,Read,Eq,Ord)
 
 newtype Bind a = Bind a
   deriving (Show,Read,Eq,Ord)
 
 data Exp =
-    Var (Name Exp)
-  | App Exp Exp
+    TT | FF
   | Pair Exp Exp
-  | TT | FF
+  | App Exp Exp
+  | Var (Name Exp)
+  | Lam (Bind Exp)
   deriving (Show,Read,Eq,Ord)
 
 type Var a = Name a -> a
 type Sig m a = Var a -> Name a -> m a
 
-class Sub a where
-  trav :: Monad m => Sig m a -> a -> m a
+class Monad m => Subst m a where
+  trav :: Sig m a -> a -> m a
 
-instance Sub Exp where
+instance Subst (State ()) Exp where
   trav s (Var nm) = s Var nm
    -- Spine nm xs = elim (s Var nm) =<< trav s xs
+  trav s TT = return TT
+  trav s FF = return FF
+  trav s (Pair a b) = do
+    a' <- trav s a
+    b' <- trav s b
+    return $ App a' b'
   trav s (App f a) = do
     f' <- trav s f
     a' <- trav s a
     return $ App f' a'
+  trav s (Lam b) = do
+    b' <- travBind s b
+    return $ Lam b'
 
--- instance Sub a => Sub (Bind a) where
---   trav s (Bind a) = undefined
+-- instance Subst a => Subst (Bind a) where
+--   trav s (Bind a) = do
+--     a' <- trav (lift s) a
+--     undefined
 
--- sub :: (Monad m,Sub a) => Bind a -> a -> a
+travBind :: Subst m a => Sig m a -> Bind a -> m (Bind a)
+travBind s (Bind a) = return . Bind =<< trav (lift s) a
+
+-- sub :: (Monad m,Subst a) => Bind a -> a -> a
 -- sub (Bind t) a = trav 
 
--- sig :: (Monad m,Sub a) => a -> Var a -> Name a -> m a
+-- sig :: (Monad m,Subst a) => a -> Var a -> Name a -> m a
 -- sig a r (Bn 0) = return a
 -- sig a r nm@(Bn i) = return (r nm)
 -- sig a r nm@(Fr _ _) = return (r nm)
 
-ren :: (Monad m,Sub a) => Sig m a -> Var a -> Name a -> m a
-ren s r nm@(Bn 0) = r nm
-ren s r nm = undefined
+lift :: Subst m a => Sig m a -> Sig m a
+lift s r nm@(Bn 0) = return $ r nm
+lift s r nm = trav wkn =<< s r nm
 
-wkn :: (Monad m,Sub a) => Sig m a
+wkn :: Subst m a => Sig m a
 wkn r (Bn i) = return $ r (Bn (succ i))
-wkn r nm@(Fr _ _) = return $ r nm
+wkn r nm@(Fr _) = return $ r nm
 
--- weakenSub :: (Monad m,Sub a) => Int -> (Name a -> m a) -> Name a -> ma
+-- weakenSub :: (Monad m,Subst a) => Int -> (Name a -> m a) -> Name a -> ma
 -- weakenSub i s nm = undefined
 
 ----------------------------------------------------------------------
