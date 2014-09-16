164b545805d84728a6edb420f2003bdc47de1deb
Larry Diehl <larrytheliquid@gmail.com>
2014-09-16 12:43:01 -0700

    Get polymemo working w00p

--- bound/PolyMemo7.hs
+++ bound/PolyMemo7.hs
@@ -17,36 +17,50 @@ import Data.Typeable
 import Data.Type.Equality
 import Control.Monad
 import Control.Monad.State
-import Prelude.Extras
+
+----------------------------------------------------------------------
 
 type Rev a = [a] -> Maybe [a]
+type ForallRev = (Typeable a , Eq a) => Rev a
+data Memo = Memo ForallRev
 
-data Memo = Memo (forall a. Eq a => Rev a)
+----------------------------------------------------------------------
 
-reverseM :: Eq a =>
+memoRev :: (Typeable a,Eq a) => ForallRev -> [a] -> [a] -> ForallRev
+memoRev f xs ys zs = case cast (xs , ys) of
+  Just (xs' , ys') -> if xs' == zs then Just ys' else f zs
+  Nothing -> f zs
+
+reverseM :: (Typeable a , Eq a) =>
   [a] -> State Memo [a]
 reverseM xs = do
   Memo f <- get
   case f xs of
     Just ys -> return ys
     Nothing -> do
-      let ys = reverse xs
-      let g = \xs' -> if xs == xs' then Just ys else f xs'
-      put (Memo g)
+      let ys = slowReverse xs
+      put (Memo (memoRev f xs ys))
       return ys
 
-    -- Just xs' -> case f xs' of
-    --   Just ys -> case cast ys of
-    --     Just ys' -> return ys'
-    --     Nothing -> error "Inconsistent type casting"
-    --   Nothing -> do
-    --     let ys = reverse xs'
-    --     let g = \xs'' -> if xs' == xs'' then Just ys else f xs'
-    --     put (Memo g)
-    --     case cast ys of
-    --       Just ys' -> return ys'
-    --       Nothing -> error "Inconsistent type casting"
-    -- Nothing -> error "Inconsistent type casting"
+reversesM :: (Typeable a , Eq a) =>
+  Int -> [a] -> State Memo [a]
+reversesM n xs | n < 1 = reverseM xs
+reversesM n xs = reverseM =<< reversesM (pred n) xs
+
+----------------------------------------------------------------------
 
 emptyMemo :: Memo
 emptyMemo = Memo (const Nothing)
+
+idMemo :: Memo
+idMemo = Memo Just
+
+----------------------------------------------------------------------
+
+slowReverse' :: Int -> [a] -> [a]
+slowReverse' n xs | n < 1 = reverse xs
+slowReverse' n xs = slowReverse' (pred n) xs
+
+slowReverse = slowReverse' 10000000
+
+----------------------------------------------------------------------
